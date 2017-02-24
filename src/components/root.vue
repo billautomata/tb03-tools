09bf1d9c93421c436932135ed65612460af1c687
Bill McKessy <bill@evident.io>
2017-02-24 12:42:43 -0800

    working!

--- src/components/root.vue
+++ src/components/root.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="root">
     <h1>TB03 tools</h1>
+    <input type="file" id="files" name="files[]" multiple />
+    <output id="list"></output>
+    <div id='patterns'></div>
   </div>
 </template>
 
@@ -9,10 +12,30 @@ export default {
   name: 'root',
   data () {
     return {}
+  },
+  mounted() {
+    console.log('mounted!')
+    require('../../lib/main.js')()
   }
 }
 </script>
 
 <style>
-
+div.pattern-parent {
+  width: 25%;
+  display: inline-block;
+}
+div.individual-step {
+  display: inline-block;
+  /*width: 6%;*/
+}
+div.step-note {
+  padding: 4px;
+}
+button.step-state {
+}
+button.step-accent {
+}
+button.step-slide {
+}
 </style>
